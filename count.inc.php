56cdfd04feda5d7bceafca1c40e17e34cc7c3daf
Daniel Silva Ferreira <danielsilva@usp.br>
2023-07-28 11:49:04 -0300

    Add files via upload

---
+++ count.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+import('lib.pkp.classes.plugins.BlockPlugin');
+
+class count extends BlockPlugin {
+
+	/**
+	 * Install default settings on system install.
+	 * @return string
+	 */
+	function getInstallSitePluginSettingsFile() {
+		return $this->getPluginPath() . '/settings.xml';
+	}
+
+	/**
+	 * Install default settings on press creation.
+	 * @return string
+	 */
+	function getContextSpecificPluginSettingsFile() {
+		return $this->getPluginPath() . '/settings.xml';
+	}
+
+	/**
+	 * Get the display name of this plugin.
+	 * @return String
+	 */
+	function getDisplayName() {
+		return __('count');
+	}
+
+	/**
+	 * Get a description of the plugin.
+	 */
+	function getDescription() {
+		return __('estatisticas omp');
+	}
+}
---
+++ templates/block.tpl
@@ -0,0 +1,18 @@
+<div class="pkp_block count">
+	<h2>Estatísticas</h2>
+	
+	<div class="content">
+
+		<b>Quantidade total de acessos: </b>{$currentPress->getViews()} <br><br> 
+		
+		<b>Quantidade total de downloads: </b>xx<br><br> 
+
+		<b>Quantidade de livros: </b>xx <br><br> 
+
+		<b>Quantidade de acessos no ano de 2023: </b>xx <br><br> 
+
+		<b>Quantidade de downloads no ano de 2023: </b>xx <br><br> 
+		
+		
+	</div>
+</div>
